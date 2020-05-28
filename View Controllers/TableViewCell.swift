311e6f16d51d8c0b366de7e948cc6a11f12d5a64
Assel <lessam07@gmail.com>
2020-05-28 18:51:16 +0600

    Added Order card layout

---
+++ View Controllers/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  CustomLoginDemo
+//
+//  Created by Assel Amangeldi on 5/25/20.
+//  Copyright © 2020 Christopher Ching. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
